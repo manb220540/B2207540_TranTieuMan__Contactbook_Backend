5338c2526012a7b8bbf7a69a08fae654d50dbe3c
manb220540 <manb2207540@student.ctu.edu.vn>
2025-06-12 16:05:46 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const contactsRouter = require('./app/routes/contact.route');
+const ApiError = require('./app/app-error');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
+
+// Middleware de xu ly loi 404
+app.use((req, res, next) => {
+    return next(new ApiError('Resource not found', 404));
+});
+// Middleware de xu ly loi chinh thuc
+app.use((err, reg, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error',
+    });
+})
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Contact Book Application!');
