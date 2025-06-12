88fee306d3297ff3ed89c4cae438215da482c0e8
manb220540 <manb2207540@student.ctu.edu.vn>
2025-06-12 15:11:24 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Contact Book Application!');
+});
+module.exports = app;
